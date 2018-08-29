32e8ecbc3eb41800a044f8f99fcfba948824f9b9
nahytar <nahytar@gmail.com>
2018-08-28 23:15:50 -0300

    crea test de busqueda de multiples links

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,13 +6,39 @@ describe('mdLinks', () => {
     expect(result).toEqual([])
   })
 
-  test ('debe devolver un link sin validar', () => {
+  test('debe devolver un link sin validar', () => {
     const result = mdLinks('test/files/oneLink.md')
     expect(result).toEqual([{
       href: 'https://www.google.com',
       text: 'I\'m an inline-style link',
       file: 'test/files/oneLink.md'
-     }])
+    }])
+  })
+
+  test('debe devolver un link sin validar', () => {
+    const result = mdLinks('test/files/multiLink.md')
+    expect(result).toEqual([
+      {
+        href: 'https://www.google.com',
+        text: 'I\'m an inline-style link',
+        file: 'test/files/multiLink.md'
+      },
+      {
+        href: 'https://www.google.com',
+        text: 'I\'m an inline-style link with title',
+        file: 'test/files/multiLink.md'
+      },
+      {
+        href: '../blob/master/LICENSE',
+        text: 'I\'m a relative reference to a repository file',
+        file: 'test/files/multiLink.md'
+      },
+      {
+        href: 'http://www.example.com',
+        text:  'http://www.example.com',
+        file: 'test/files/multiLink.md'
+      }
+    ])
   })
 })
 
