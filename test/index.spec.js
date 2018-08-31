b7848a1bcf84ee200ac9e555ba5631eaa2cc7738
nahytar <nahytar@gmail.com>
2018-08-31 00:54:57 -0300

    se agrega validacion de links

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,44 +1,102 @@
 const mdLinks = require('../lib/mdlink').mdLinks;
 
 describe('mdLinks', () => {
-  test('debe devolver arreglo vacio', () => {
-    const result = mdLinks('test/files/empty.md')
-    expect(result).toEqual([])
+  test('debe devolver arreglo vacio', (done) => {
+    mdLinks('test/files/empty.md').then((result) => {
+      expect(result).toEqual([]);
+      done();
+    })
   })
 
-  test('debe devolver un link sin validar', () => {
-    const result = mdLinks('test/files/oneLink.md')
-    expect(result).toEqual([{
-      href: 'https://www.google.com',
-      text: 'I\'m an inline-style link',
-      file: 'test/files/oneLink.md'
-    }])
-  })
-
-  test('debe devolver un link sin validar', () => {
-    const result = mdLinks('test/files/multiLink.md')
-    expect(result).toEqual([
-      {
+  test('debe devolver un link sin validar', (done) => {
+    mdLinks('test/files/oneLink.md').then((result) => {
+      expect(result).toEqual([{
         href: 'https://www.google.com',
         text: 'I\'m an inline-style link',
-        file: 'test/files/multiLink.md'
-      },
-      {
-        href: 'https://www.google.com',
-        text: 'I\'m an inline-style link with title',
-        file: 'test/files/multiLink.md'
-      },
-      {
-        href: '../blob/master/LICENSE',
-        text: 'I\'m a relative reference to a repository file',
-        file: 'test/files/multiLink.md'
-      },
-      {
-        href: 'http://www.example.com',
-        text:  'http://www.example.com',
-        file: 'test/files/multiLink.md'
-      }
-    ])
+        file: 'test/files/oneLink.md'
+      }]);
+      done();
+    })
+  })
+
+  test('debe devolver un link sin validar', (done) => {
+    mdLinks('test/files/multiLink.md').then((result) => {
+      expect(result).toEqual([
+        {
+          href: 'https://www.google.com',
+          text: 'I\'m an inline-style link',
+          file: 'test/files/multiLink.md'
+        },
+        {
+          href: 'https://www.google.com',
+          text: 'I\'m an inline-style link with title',
+          file: 'test/files/multiLink.md'
+        },
+        {
+          href: '../blob/master/LICENSE',
+          text: 'I\'m a relative reference to a repository file',
+          file: 'test/files/multiLink.md'
+        },
+        {
+          href: 'http://www.example.com',
+          text: 'http://www.example.com',
+          file: 'test/files/multiLink.md'
+        }
+      ]);
+      done();
+    })
+  })
+  describe('con validacion', () => {
+    test('debe devolver arreglo vacio', (done) => {
+      mdLinks('test/files/empty.md', { validate: true }).then((result) => {
+        expect(result).toEqual([]);
+        done();
+      })
+    })
+
+    test('debe devolver un link sin validar', (done) => {
+      mdLinks('test/files/oneLink.md', { validate: true }).then((result) => {
+        expect(result).toEqual([{
+          href: 'https://www.google.com',
+          text: 'I\'m an inline-style link',
+          file: 'test/files/oneLink.md',
+          status: 'ok'
+        }]);
+        done();
+      })
+    })
+
+    test('debe devolver un link sin validar', (done) => {
+      mdLinks('test/files/multiLink.md', { validate: true }).then((result) => {
+        expect(result).toEqual([
+          {
+            href: 'https://www.google.com',
+            text: 'I\'m an inline-style link',
+            file: 'test/files/multiLink.md',
+            status: 'ok'
+          },
+          {
+            href: 'https://www.google.com',
+            text: 'I\'m an inline-style link with title',
+            file: 'test/files/multiLink.md',
+            status: 'ok'
+          },
+          {
+            href: '../blob/master/LICENSE',
+            text: 'I\'m a relative reference to a repository file',
+            file: 'test/files/multiLink.md',
+            status: 'fail'
+          },
+          {
+            href: 'http://www.example.com',
+            text: 'http://www.example.com',
+            file: 'test/files/multiLink.md',
+            status: 'ok'
+          }
+        ]);
+        done();
+      })
+    })
   })
 })
 
